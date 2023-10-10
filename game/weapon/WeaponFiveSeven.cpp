80bfeeb1f18d9c33caf33f5868b6a8a74f1cfd36
Timothy Estrada <43392928+timo1227@users.noreply.github.com>
2023-10-10 15:27:47 -0400

    FiveSeven Gun Complete

--- game/weapon/WeaponFiveSeven.cpp
+++ game/weapon/WeaponFiveSeven.cpp
@@ -21,24 +21,17 @@ public:
 
 protected:
 
-	bool				UpdateAttack(void);
+	bool				fireHeld;
 	bool				UpdateFlashlight	( void );
 	void				Flashlight			( bool on );
 
 private:
 
-	int					chargeTime;
-	int					chargeDelay;
-	idVec2				chargeGlow;
-	bool				fireForced;
-	int					fireHeldTime;
-
 	stateResult_t		State_Raise				( const stateParms_t& parms );
 	stateResult_t		State_Lower				( const stateParms_t& parms );
 	stateResult_t		State_Idle				( const stateParms_t& parms );
-	stateResult_t		State_Charge			( const stateParms_t& parms );
-	stateResult_t		State_Charged			( const stateParms_t& parms );
 	stateResult_t		State_Fire				( const stateParms_t& parms );
+	stateResult_t		State_Reload			( const stateParms_t& parms );
 	stateResult_t		State_Flashlight		( const stateParms_t& parms );
 	
 	CLASS_STATES_PROTOTYPE ( rvWeaponFiveSeven );
@@ -88,65 +81,13 @@ void rvWeaponFiveSeven::Flashlight ( bool on ) {
 
 /*
 ================
-rvWeaponBlaster::UpdateAttack
-================
-*/
-bool rvWeaponFiveSeven::UpdateAttack(void) {
-	// Clear fire forced
-	if (fireForced) {
-		if (!wsfl.attack) {
-			fireForced = false;
-		}
-		else {
-			return false;
-		}
-	}
-
-	// If the player is pressing the fire button and they have enough ammo for a shot
-	// then start the shooting process.
-	if (wsfl.attack && gameLocal.time >= nextAttackTime) {
-		// Save the time which the fire button was pressed
-		if (fireHeldTime == 0) {
-			nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier(PMOD_FIRERATE));
-			fireHeldTime = gameLocal.time;
-			viewModel->SetShaderParm(BLASTER_SPARM_CHARGEGLOW, chargeGlow[0]);
-		}
-	}
-
-	// If they have the charge mod and they have overcome the initial charge 
-	// delay then transition to the charge state.
-	if (fireHeldTime != 0) {
-		// If the fire button was let go but was pressed at one point then 
-		// release the shot.
-		if (!wsfl.attack) {
-			idPlayer* player = gameLocal.GetLocalPlayer();
-			if (player) {
-
-				if (player->GuiActive()) {
-					//make sure the player isn't looking at a gui first
-					SetState("Lower", 0);
-				}
-				else {
-					SetState("Fire", 0);
-				}
-			}
-			return true;
-		}
-	}
-
-	return false;
-}
-
-/*
-================
 rvWeaponFiveSeven::Spawn
 ================
 */
 void rvWeaponFiveSeven::Spawn ( void ) {
-	SetState ( "Raise", 0 );
+	fireHeld = false;
 
-	fireHeldTime		= 0;
-	fireForced			= false;
+	SetState ( "Raise", 0 );
 			
 	Flashlight ( owner->IsFlashlightOn() );
 }
@@ -157,8 +98,7 @@ rvWeaponFiveSeven::Save
 ================
 */
 void rvWeaponFiveSeven::Save ( idSaveGame *savefile ) const {
-	savefile->WriteBool ( fireForced );
-	savefile->WriteInt ( fireHeldTime );
+	savefile->WriteBool(fireHeld);
 }
 
 /*
@@ -167,8 +107,7 @@ rvWeaponFiveSeven::Restore
 ================
 */
 void rvWeaponFiveSeven::Restore ( idRestoreGame *savefile ) {
-	savefile->ReadBool ( fireForced );
-	savefile->ReadInt ( fireHeldTime );
+	savefile->ReadBool(fireHeld);
 }
 
 /*
@@ -207,9 +146,8 @@ CLASS_STATES_DECLARATION ( rvWeaponFiveSeven )
 	STATE ( "Raise",						rvWeaponFiveSeven::State_Raise )
 	STATE ( "Lower",						rvWeaponFiveSeven::State_Lower )
 	STATE ( "Idle",							rvWeaponFiveSeven::State_Idle)
-	STATE(	"Charge",						rvWeaponFiveSeven::State_Charge)
-	STATE(	"Charged",						rvWeaponFiveSeven::State_Charged)
 	STATE ( "Fire",							rvWeaponFiveSeven::State_Fire )
+	STATE ( "Reload",						rvWeaponFiveSeven::State_Reload )
 	STATE ( "Flashlight",					rvWeaponFiveSeven::State_Flashlight )
 END_CLASS_STATES
 
@@ -286,96 +224,53 @@ stateResult_t rvWeaponFiveSeven::State_Idle ( const stateParms_t& parms ) {
 	enum {
 		IDLE_INIT,
 		IDLE_WAIT,
-	};	
-	switch ( parms.stage ) {
-		case IDLE_INIT:			
-			SetStatus ( WP_READY );
-			PlayCycle( ANIMCHANNEL_ALL, "idle", parms.blendFrames );
-			return SRESULT_STAGE ( IDLE_WAIT );
-			
+	};
+	switch (parms.stage) {
+		case IDLE_INIT:
+			if (!AmmoAvailable()) {
+				SetStatus(WP_OUTOFAMMO);
+			}
+			else {
+				SetStatus(WP_READY);
+			}
+
+			PlayCycle(ANIMCHANNEL_ALL, "idle", parms.blendFrames);
+			return SRESULT_STAGE(IDLE_WAIT);
+
 		case IDLE_WAIT:
-			if ( wsfl.lowerWeapon ) {
-				SetState ( "Lower", 4 );
+			if (wsfl.lowerWeapon) {
+				SetState("Lower", 4);
 				return SRESULT_DONE;
 			}
-			
-			if ( UpdateFlashlight ( ) ) { 
+			if (UpdateFlashlight()) {
 				return SRESULT_DONE;
 			}
-			if (UpdateAttack()) {
-				return SRESULT_DONE;
+
+			if (fireHeld && !wsfl.attack) {
+				fireHeld = false;
+			}
+			if (!clipSize) {
+				if (!fireHeld && gameLocal.time > nextAttackTime && wsfl.attack && AmmoAvailable()) {
+					SetState("Fire", 0);
+					return SRESULT_DONE;
+				}
+			}
+			else {
+				if (!fireHeld && gameLocal.time > nextAttackTime && wsfl.attack && AmmoInClip ( ) ) {
+					SetState("Fire", 0);
+					return SRESULT_DONE;
+				}
+				if (wsfl.attack && AutoReload() && !AmmoInClip() && AmmoAvailable()) {
+					SetState("Reload", 4);
+					return SRESULT_DONE;
+				}
+				if (wsfl.netReload || (wsfl.reload && AmmoInClip() < ClipSize() && AmmoAvailable() > AmmoInClip())) {
+					SetState("Reload", 4);
+					return SRESULT_DONE;
+				}
 			}
 			return SRESULT_WAIT;
-
-	}
-	return SRESULT_ERROR;
-}
-
-/*
-================
-rvWeaponBlaster::State_Charge
-================
-*/
-stateResult_t rvWeaponFiveSeven::State_Charge(const stateParms_t & parms) {
-	enum {
-		CHARGE_INIT,
-		CHARGE_WAIT,
-	};
-	switch (parms.stage) {
-	case CHARGE_INIT:
-		viewModel->SetShaderParm(BLASTER_SPARM_CHARGEGLOW, chargeGlow[0]);
-		StartSound("snd_charge", SND_CHANNEL_ITEM, 0, false, NULL);
-		PlayCycle(ANIMCHANNEL_ALL, "charging", parms.blendFrames);
-		return SRESULT_STAGE(CHARGE_WAIT);
-
-	case CHARGE_WAIT:
-		if (gameLocal.time - fireHeldTime < chargeTime) {
-			float f;
-			f = (float)(gameLocal.time - fireHeldTime) / (float)chargeTime;
-			f = chargeGlow[0] + f * (chargeGlow[1] - chargeGlow[0]);
-			f = idMath::ClampFloat(chargeGlow[0], chargeGlow[1], f);
-			viewModel->SetShaderParm(BLASTER_SPARM_CHARGEGLOW, f);
-
-			if (!wsfl.attack) {
-				SetState("Fire", 0);
-				return SRESULT_DONE;
-			}
-
-			return SRESULT_WAIT;
 		}
-		SetState("Charged", 4);
-		return SRESULT_DONE;
-	}
-	return SRESULT_ERROR;
-}
-
-/*
-================
-rvWeaponBlaster::State_Charged
-================
-*/
-stateResult_t rvWeaponFiveSeven::State_Charged(const stateParms_t& parms) {
-	enum {
-		CHARGED_INIT,
-		CHARGED_WAIT,
-	};
-	switch (parms.stage) {
-	case CHARGED_INIT:
-		viewModel->SetShaderParm(BLASTER_SPARM_CHARGEGLOW, 1.0f);
-
-		StopSound(SND_CHANNEL_ITEM, false);
-		StartSound("snd_charge_loop", SND_CHANNEL_ITEM, 0, false, NULL);
-		StartSound("snd_charge_click", SND_CHANNEL_BODY, 0, false, NULL);
-		return SRESULT_STAGE(CHARGED_WAIT);
-
-	case CHARGED_WAIT:
-		if (!wsfl.attack) {
-			fireForced = true;
-			SetState("Fire", 0);
-			return SRESULT_DONE;
-		}
-		return SRESULT_WAIT;
-	}
 	return SRESULT_ERROR;
 }
 
@@ -393,6 +288,7 @@ stateResult_t rvWeaponFiveSeven::State_Fire ( const stateParms_t& parms ) {
 		case FIRE_INIT:	
 
 			StopSound ( SND_CHANNEL_ITEM, false );
+			nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier(PMOD_FIRERATE));
 
 			//don't fire if we're targeting a gui.
 			idPlayer* player;
@@ -400,22 +296,19 @@ stateResult_t rvWeaponFiveSeven::State_Fire ( const stateParms_t& parms ) {
 
 			//make sure the player isn't looking at a gui first
 			if( player && player->GuiActive() )	{
-				fireHeldTime = 0;
 				SetState ( "Lower", 0 );
 				return SRESULT_DONE;
 			}
 
 			if( player && !player->CanFire() )	{
-				fireHeldTime = 0;
 				SetState ( "Idle", 4 );
 				return SRESULT_DONE;
 			}
-
-			Attack ( false, 1, spread, 0, 1.0f );
-			PlayEffect ( "fx_normalflash", barrelJointView, false );
-			PlayAnim( ANIMCHANNEL_ALL, "fire", parms.blendFrames );
-
-			fireHeldTime = 0;
+			
+			Attack(false, 1, spread, 0, 1.0f);
+			PlayEffect("fx_normalflash", barrelJointView, false);
+			fireHeld = true;
+			PlayAnim(ANIMCHANNEL_ALL, "fire", parms.blendFrames);
 			
 			return SRESULT_STAGE(FIRE_WAIT);
 		
@@ -424,11 +317,49 @@ stateResult_t rvWeaponFiveSeven::State_Fire ( const stateParms_t& parms ) {
 				SetState ( "Idle", 4 );
 				return SRESULT_DONE;
 			}
-			if (UpdateFlashlight() || UpdateAttack()) {
+			if (UpdateFlashlight()) {
 				return SRESULT_DONE;
 			}
 			return SRESULT_WAIT;
 	}			
+	return SRESULT_ERROR;
+}
+
+/*
+================
+rvWeaponFiveSeven::State_Reload
+================
+*/
+stateResult_t rvWeaponFiveSeven::State_Reload(const stateParms_t& parms) {
+	enum {
+		STAGE_INIT,
+		STAGE_WAIT,
+	};
+	switch (parms.stage) {
+	case STAGE_INIT:
+		if (wsfl.netReload) {
+			wsfl.netReload = false;
+		}
+		else {
+			NetReload();
+		}
+
+		SetStatus(WP_RELOAD);
+		PlayAnim(ANIMCHANNEL_ALL, "reload", parms.blendFrames);
+		return SRESULT_STAGE(STAGE_WAIT);
+
+	case STAGE_WAIT:
+		if (AnimDone(ANIMCHANNEL_ALL, 4)) {
+			AddToClip(ClipSize());
+			SetState("Idle", 4);
+			return SRESULT_DONE;
+		}
+		if (wsfl.lowerWeapon) {
+			SetState("Lower", 4);
+			return SRESULT_DONE;
+		}
+		return SRESULT_WAIT;
+	}
 	return SRESULT_ERROR;
 }
 
